e43257edd48e2ae53eb520ea02d6737a3dd429da
BillyBoyo <john@doe.com>
2023-06-30 16:18:04 +0200

    pret pour la soutenance

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -27,12 +27,27 @@ add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 
 add_filter( 'wpcf7_autop_or_not', '__return_false' );  //enleve les balises <p> du contact form7
 
+
+add_filter('wpcf7_form_elements', function($content) {      //enleve les balises span du contact form 7
+    $content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
+
+    return $content;
+});
+
+
+
+
+// HOOK admin
 function ajout_lien_admin($items, $args) {
-    if (is_user_logged_in())  {
+    if (is_user_logged_in() && !is_footer_menu($args->theme_location))  {
         $lien_admin = '<li class="menu-item bouton-admin"><a href="' . admin_url() . '">Admin</a></li>';
         $items .= $lien_admin;
-        remove_filter('wp_nav_menu_items', 'ajout_lien_admin', 10, 2); // Supprime le filtre après avoir ajouté le bouton
     }
     return $items;
 }
 add_filter('wp_nav_menu_items', 'ajout_lien_admin', 10, 2);
+
+function is_footer_menu($theme_location) {
+    // Vérifie si le thème location est le footer menu
+    return $theme_location === 'footer_menu';
+}
